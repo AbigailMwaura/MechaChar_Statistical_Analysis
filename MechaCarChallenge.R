f5efdf5203e70fcaf313b1a0e20c1c76a799e276
Abigail Mwaura <amwaura60@gmail.com>
2021-04-20 12:22:42 -0400

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,7 @@ t.test(MechaCar_Suspension$PSI,mu=1500)
 Lot1<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot1")
 Lot2<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot2")
 Lot3<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot3")
+
+t.test(Lot1$PSI,mu=1500)
+t.test(Lot2$PSI,mu=1500)
+t.test(Lot3$PSI,mu=1500)
